8b698e46f2303ca7cdb8de53831c78c9308afa53
Ryan Batchelor <121133330+RyanBatchelor@users.noreply.github.com>
2023-09-06 14:15:12 +0000

    Update 3 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
+import styled from "styled-components"
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600px
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+`;
+
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <H1>Batchelor Apartments</H1>
+      <Button onClick={()=>alert("Check In")}>Check in</Button>
+    </div>
   )
 }
 
