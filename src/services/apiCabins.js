8a495d282212811d12e7d09d235be57643566ca6
Ryan Batchelor <121133330+RyanBatchelor@users.noreply.github.com>
2023-09-07 15:36:38 +0000

    Can create new listing

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -14,17 +14,29 @@ export async function getCabins(){
   return data
 }
 
+export async function CreateCabin(newCabin){
+
+  const { data, error } = await supabase
+    .from('cabins')
+    .insert([newCabin])
+    if(error){
+        console.error(error)
+        throw new Error('Cabins could not be created')
+      }
+      return data
+}
+
 export async function deleteCabin(id){
   
-const { data, error } = await supabase
-  .from('cabins')
-  .delete()
-  .eq('id', id)
+  const { data, error } = await supabase
+    .from('cabins')
+    .delete()
+    .eq('id', id)
 
-  if(error){
-      console.error(error)
-      throw new Error('Cabins could not be deleted')
-    }
-    return data
+    if(error){
+        console.error(error)
+        throw new Error('Cabins could not be deleted')
+      }
+      return data
 }
 
